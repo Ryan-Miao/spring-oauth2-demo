ba435d0947ab8c74bd81e6f2ac46e232f25cc728
ryan.miao <ryan.miao@nf-3.com>
2018-06-22 14:50:22 +0800

    Initial commit

---
+++ resource/src/main/java/com/test/oauth2/resource/ResourceApplication.java
@@ -0,0 +1,16 @@
+package com.test.oauth2.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ryan Miao at 2018-06-21 17:45
+ **/
+@SpringBootApplication
+public class ResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceApplication.class, args);
+    }
+
+}
---
+++ server/src/main/java/com/test/oauth2/server/Oauth2ServerApplication.java
@@ -0,0 +1,16 @@
+package com.test.oauth2.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ryan Miao at 2018-06-21 17:36
+ **/
+@SpringBootApplication
+public class Oauth2ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ServerApplication.class, args);
+    }
+
+}
